ebc28c1decc300d612af0c26bb263c75e15296f4
Scorp1Oz <111981572+Scorp1Oz@users.noreply.github.com>
2023-10-16 23:28:31 +0300

    Update Project.java

--- Project.java
+++ Project.java
@@ -2,6 +2,7 @@ public class Project {
 
         public static void main(String[] args) {
             System.out.println("Hello, World!");
+	    System.out.println("Hello, World... Again!!");
         }
 
 }
